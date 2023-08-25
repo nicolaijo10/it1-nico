55d809f744002d3d7957f0ade77e8090d2517973
nicolaijo10 <floweykeeper2005@gmail.com>
2023-08-25 10:23:19 +0200

    Oppgave arbeid

---
+++ IT2/start/Kappitel 1 start/oppgaver B.py
@@ -0,0 +1,67 @@
+
+'''
+tall1 = int(input("Oppgi tall 1: "))
+
+minst = tall1
+
+tall2 = int(input("Oppgi tall 2: "))
+
+if (tall2 < minst):
+  minst = tall2
+
+tall3 = int(input("Oppgi tall 3: "))
+
+if (tall3 < minst):
+  minst = tall3
+
+tall4 = int(input("oppgi tall 4: "))
+
+if (tall4 < minst):
+  minst = tall4
+
+tall5 = int(input("oppgi tall 5: "))
+
+if (tall5 < minst):
+  minst = tall5
+
+print(f"Det minste tallet var {minst}.")
+
+'''
+
+'''
+tall = 69
+
+if tall > 50 and tall < 100:
+  print("Tallet er mellom 50 og 100")
+else:
+  print("Tallet er ikke mellom 50 og 100")
+
+'''
+
+'''
+a = 1
+b = 0
+
+if a > 0 or a < 0:
+    print("Tall (a) er ikke lik 0")
+else:
+    print("Tall (a) er lik 0")
+
+if b > 0 or b < 0:
+    print("Tall (a) er ikke lik 0")
+else:
+    print("Tall (b) er lik 0")
+
+'''
+
+'''
+heltall = int(input("skriv inn et heltall"))
+
+if heltall%2==0:
+    print("tallet er et partall")
+else:
+    print("tallet er et oddetall")
+
+'''
+
+
