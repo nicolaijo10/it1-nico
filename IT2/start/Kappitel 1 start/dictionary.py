d7d87842ac3d31e5744ba57813fbfe12b24f5a1a
nicolaijo10 <floweykeeper2005@gmail.com>
2023-09-15 10:21:47 +0200

    Oppgave arbeid

--- IT2/start/Kappitel 1 start/dictionary.py
+++ IT2/start/Kappitel 1 start/dictionary.py
@@ -81,4 +81,91 @@ for ol in sommer_ol:
 
 #Blandede oppgaver
 
+'''
+tall_tekst = {
+    1: "en", 
+    2: "to", 
+    3: "tre", 
+    4: "fire", 
+    5: "fem", 
+    6: "seks", 
+    7: "syv", 
+    8: "åtte", 
+    9: "ni", 
+    10: "ti"
+}
+
+print("Tall | Tekst")
+print("-----|------")
+for tall, tekst in tall_tekst.items():
+    print(f"{tall:<5}| {tekst}")
+'''
+
+'''
+handlekurv = {
+   "melk": 17.90,
+   "smør": 38.90,
+   "kokt skinke": 23.10,
+   "sjokolade": 11.90,
+   "oppvaskmiddel": 24.40,
+   "frossenpizza": 29.90
+}
+
+# Finner laveste pris og tilhørende produkt
+laveste_pris_produkt = min(handlekurv, key = handlekurv.get)
+laveste_pris = handlekurv[laveste_pris_produkt]
+
+# Finner høyeste pris og tilhørende produkt
+høyeste_pris_produkt = max(handlekurv, key = handlekurv.get)
+høyeste_pris = handlekurv[høyeste_pris_produkt]
+
+# Finner totalprisen
+totalpris = sum(handlekurv.values())
+
+# Skriver ut resultatene
+print("Laveste pris:", laveste_pris_produkt, laveste_pris)
+print("Høyeste pris:", høyeste_pris_produkt, høyeste_pris)
+print("Totalpris:", totalpris)
+
+'''
+
+
+'''
+# Ordbok over norske byer
+norske_byer = {
+    "Oslo": {
+        "Befolkning": 693494,
+        "Beliggenhet": "Østlandet",
+        "Kjente attraksjoner": ["Operahuset", "Vigelandsparken", "Akershus festning"]
+    },
+    "Bergen": {
+        "Befolkning": 280216,
+        "Beliggenhet": "Vestlandet",
+        "Kjente attraksjoner": ["Bryggen", "Fløibanen", "Akvariet i Bergen"]
+    },
+    "Trondheim": {
+        "Befolkning": 198711,
+        "Beliggenhet": "Trøndelag",
+        "Kjente attraksjoner": ["Nidarosdomen", "Gamle Bybro", "Rockheim"]
+    },
+    "Stavanger": {
+        "Befolkning": 143574,
+        "Beliggenhet": "Rogaland",
+        "Kjente attraksjoner": ["Preikestolen", "Stavanger domkirke", "Oljemuseet"]
+    },
+    "Tromsø": {
+        "Befolkning": 76703,
+        "Beliggenhet": "Troms og Finnmark",
+        "Kjente attraksjoner": ["Ishavskatedralen", "Polaria", "Tromsøya"]
+    }
+}
+
+# Skriv ut informasjonen om alle byene
+for by, info in norske_byer.items():
+    print(f"By: {by}")
+    print(f"Befolkning: {info['Befolkning']}")
+    print(f"Beliggenhet: {info['Beliggenhet']}")
+    print(f"Kjente attraksjoner: {', '.join(info['Kjente attraksjoner'])}")
+    print("\n")
+'''
 
