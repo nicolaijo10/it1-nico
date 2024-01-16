63aedfbcfb764bc6c05b0903646067b3592ab135
nicolaijo10 <floweykeeper2005@gmail.com>
2024-01-16 15:46:20 +0100

    prosjekt arbeid

--- IT2/start/pygame/Prosjekt(30 januar)/prosjektV3.py
+++ IT2/start/pygame/Prosjekt(30 januar)/prosjektV3.py
@@ -1,12 +1,12 @@
 import pygame as pg
-from pygame.locals import (K_UP, K_DOWN, K_RIGHT, K_LEFT)
+from pygame.locals import (K_UP, K_DOWN, K_RIGHT, K_LEFT, K_w, K_s)
 import math as m
 
 pg.init()
 
 # Oppretter et vindu der vi skal "tegne" innholdet vårt
-VINDU_BREDDE = 1000
-VINDU_HOYDE = 1000
+VINDU_BREDDE = 1920
+VINDU_HOYDE = 1080
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 
 class objekter():
@@ -25,6 +25,7 @@ class objekter():
         rect = (self.x, self.y, self.width, self.height)
         pg.draw.rect(self.vindusobjekt, self.farge, rect)
 
+'''
     def finnAvstand(self, annetObjekt):
         """Metode for å finne avstanden til en annen ball eller spiller"""
         xAvstand2 = (self.x - annetObjekt.x) ** 2  # x-avstand i andre
@@ -40,7 +41,7 @@ class objekter():
     def kollisjon(self, annetObjekt):
         """Metode for å sjekke kollisjon med et annet objekt"""
         return self.finnAvstand(annetObjekt) <= 0
-
+'''     
 class Ball(objekter):
     def __init__(self, x, y, radius, farge, vindusobjekt, xFart, yFart):
         super().__init__(x, y, radius, farge, vindusobjekt, (0, 0))  # Corrected order
@@ -66,18 +67,41 @@ class Spiller(objekter):
 
     def flytt(self, taster):
         """Metode for å flytte spilleren"""
-        if taster[K_UP]:
+        if taster[K_w]:
             self.y -= self.fart
-        if taster[K_DOWN]:
+        if taster[K_s]:
             self.y += self.fart
         if taster[K_RIGHT]:
             self.x += self.fart
         if taster[K_LEFT]:
             self.x -= self.fart
             
+    def collide_with(self, ball):
+        paddle_rect = pg.Rect(self.x, self.y, self.width, self.height)
+        ball_rect = pg.Rect(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2)
+        return paddle_rect.colliderect(ball_rect)
 
-ball = Ball(200, 200, 20, (255, 0, 0), vindu, 2, 2)
-spiller1 = Spiller(120, 50, 0, 20, 200, (0, 0, 250), vindu, 2)
+class Spiller2(objekter):
+    def __init__(self, x, y, radius, height, width, farge, vindusobjekt, fart):
+        super().__init__(x, y, radius, farge, vindusobjekt, (height, width))
+        self.fart = fart
+
+    def flytt(self, taster):
+        """Metode for å flytte spilleren"""
+        if taster[K_UP]:
+            self.y -= self.fart
+        if taster[K_DOWN]:
+            self.y += self.fart
+            
+    def collide_with(self, ball):
+        paddle_rect = pg.Rect(self.x, self.y, self.width, self.height)
+        ball_rect = pg.Rect(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2)
+        return paddle_rect.colliderect(ball_rect)
+
+ball = Ball(200, 200, 20, (255, 0, 0), vindu, 2.5, 2.5)
+spiller1 = Spiller(120, 50, 0, 20, 200, (0, 0, 250), vindu, 3)
+spiller2 = Spiller2(1800, 50, 0, 20, 200, (0, 0, 250), vindu, 3)
+score_font = pg.font.Font(None, 36)
 
 fortsett = True
 while fortsett:
@@ -92,14 +116,18 @@ while fortsett:
     ball.flytt(spiller1)
     spiller1.tegnRectangle()
     spiller1.flytt(trykkede_taster)
+    spiller2.tegnRectangle()
+    spiller2.flytt(trykkede_taster)
 
-    avstand = spiller1.finnAvstand(ball)
-    if avstand <= 10:
-        # Just for testing, you can adjust the factor based on your preference
+    if spiller1.collide_with(ball):
+        ball.xFart *= -1.0
+        ball.yFart *= 1.0
+        
+
+    if spiller2.collide_with(ball):
         ball.xFart *= -1.0
         ball.yFart *= 1.0
 
-    print(avstand)
     pg.display.flip()
 
 pg.quit()
